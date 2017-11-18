68e0fc105b13dfc040c0c0b1790b6406f0a6b6ed
jeremy <oxonwater@hotmail.com>
2017-11-18 17:40:21 -0500

    Added docstrings to model_generator

--- model_generator.py
+++ model_generator.py
@@ -12,6 +12,34 @@ from timeit import default_timer as timer
 
 
 def create_model_from_city(city_name, save_file, high_res=True, max_calls_per_key=2250, gmap_keys):
+    """
+    Creates a multidigraph networkx model of a city, and uses gpickle to save it.
+    
+    Parameters:
+    -----------
+    city_name: string
+        The name of the city you want a model of. E.g. “Amherst, MA”
+    save_file: string
+        The name of the file you want to save the networkx graph to.
+    high_res: Boolean, default=True
+        If False, elevation gain between nodes will be based solely off of the 
+        elevation at each node.
+        If True, ATTEMPT to sample elevation gains along every edge to get a 
+        higher resolution of elevation gain per edge. If there are not enough 
+        API calls to get samples, the lower resolution (using just the nodes) 
+        is substituted.
+    max_api_calls_per_key: int, default=2250
+        The maximum number of Google maps API calls that will be used for each 
+        key in gmap_keys. Free Google map API keys limit API requests to 2500 
+        calls per day, the default 2250 is a conservative lower bound on this 
+        maximum. 
+    gmap_keys: list of strings
+        Each string should contain a Google maps API key.
+        
+    Returns:
+    --------
+        city_graph: NetworkX MultiDiGraph
+    """
     # Calculate total number of api calls usable
     api_calls_left = len(gmap_keys) * max_calls_per_key
     # Create networkx mutlidigraph of city
@@ -30,6 +58,18 @@ def create_model_from_city(city_name, save_file, high_res=True, max_calls_per_ke
     
 
 def osmnx_graph_from_place(city_name):
+    """
+    Calls OSMnx to get a networkx multidigraph representation of a city.
+    
+    Parameters:
+    -----------
+    city_name: string
+        The name of the city you want a graph of. E.g. “Amherst, MA”
+        
+    Returns:
+    --------
+    city_graph: NetworkX MultiDiGraph
+    """
     print("Initializing Graph of {} with OSMnx...".format(city_name))
     start = timer()
     
@@ -42,9 +82,26 @@ def osmnx_graph_from_place(city_name):
 
 
 def filter_edge_attributes(graph, attributes_to_keep):
+    """
+    Removes all edge attributes not listed from a NetworkX MultiDiGraph
+    
+    Parameters:
+    -----------
+    graph: NetworkX MultiDiGraph
+        The graph you wish to filter edge attributes from.
+    attributes_to_keep: list of strings
+        A list of edge attributes to keep.
+        
+    Returns:
+    --------
+    graph: NetworkX MultiDiGraph
+        The input graph with edges filtered.
+    """
+    
     print("Removing all edge attributes from {} besides {}...".format(graph, attributes_to_keep))
     start = timer()
-    
+    # For each edge find which attributes are not in attributes to keep,
+    # and delete them from the dict.
     for u, v, k, data in graph.edges(keys=True, data=True):
         attributes_to_del = []
         for attribute in graph[u][v][k]:
@@ -61,6 +118,36 @@ def filter_edge_attributes(graph, attributes_to_keep):
 
 
 def add_node_elevation_data(graph, gmap_clients, api_calls_left, max_calls_per_key, max_nodes_per_call=350):
+    """
+    Add elevation and resolution as attributes to every node using Google Maps 
+    API calls.
+    
+    Parameters:
+    -----------
+    graph: NetworkX MultiDiGraph
+        The graph you wish to add elevation data to. This assumes each node has
+        a y and x attribute, and a geometry attribute if the road is not 
+        straight, as given from OSMnx.
+    gmap_clients: list of Googlemaps.client
+        Clients set up with the python package googlemaps using Google maps 
+        API keys.
+    api_calls_left: int
+        Number of google maps API calls not used yet from the gmap_clients, 
+        assuming max_api_calls_per_key.
+    max_nodes_per_call: int, default=350
+        The maximum number of nodes for which elevation data will be requested 
+        with a single API call. Google maps elevation API limits the requests 
+        to a character limit, and the default 350 nodes is a conservative 
+        estimate for this limit.
+    max_api_calls_per_key: int
+        The maximum number of Google maps API calls that will be used for each 
+        key in gmap_keys.
+        
+    Returns:
+    --------
+    api_calls_left: int
+        The input api_calls_left minus the number of api calls used.
+    """
     print("Adding node elevation data from Google Maps Elevation API...")
     start = timer()
     
@@ -99,6 +186,44 @@ def add_node_elevation_data(graph, gmap_clients, api_calls_left, max_calls_per_k
 
 
 def add_elevation_data(graph, gmap_clients, api_calls_left, max_calls_per_key, high_res):
+    """
+    Add elevation and resolution as attributes to every node using Google Maps 
+    API calls. Then add ele_gain attribute to every edge by sampling along the 
+    road using Google Maps API calls, or by calculating the elevation gain 
+    using each node's elevation attribute.
+    Note: A node's resolution attribute is used to estimate the resolution we 
+          can expect from Google Maps API, and influences our decision for 
+          the number of samples along the edge between two nodes.
+    
+    
+    Parameters:
+    -----------
+    graph: NetworkX MultiDiGraph
+        The graph you wish to add elevation data to. This assumes each node has
+        a y and x attribute, and a geometry attribute if the road is not 
+        straight, as given from OSMnx.
+    gmap_clients: list of Googlemaps.client
+        Clients set up with the python package googlemaps using Google maps 
+        API keys.
+    api_calls_left: int
+        Number of google maps API calls not used yet from the gmap_clients, 
+        assuming max_api_calls_per_key.
+    max_api_calls_per_key: int
+        The maximum number of Google maps API calls that will be used for each 
+        key in gmap_keys.
+    high_res: Boolean
+        If False, elevation gain between nodes will be based solely off of the 
+        elevation at each node.
+        If True, ATTEMPT to sample elevation gains along every edge to get a 
+        higher resolution of elevation gain per edge. If there are not enough 
+        API calls to get samples, the lower resolution (using just the nodes) 
+        is substituted.
+        
+    Returns:
+    --------
+    api_calls_left: int
+        The input api_calls_left minus the number of api calls used.
+    """
     print("Adding elevation data to graph from Google Maps Elevation API...")
     start = timer()
     # Add elevation data at each node, and update api_calls_left
@@ -159,6 +284,21 @@ def add_elevation_data(graph, gmap_clients, api_calls_left, max_calls_per_key, h
 
 
 def sum_ele_gains(elevation_list, backwards=False):
+    """
+    Sums the sequential gains between each element in elevation_list    
+    
+    Parameters:
+    -----------
+    elevation_list: list of floats
+        The list of elevations along a road
+    backwards: boolean, default=False
+        weither or not to sum the gains forward or backwards
+        
+    Returns:
+    --------
+    gain: float
+        The gain summed over elevation_list.
+    """
     # Reverse list if backwards is true
     if backwards:
         elevation_list = elevation_list[::-1]
@@ -174,6 +314,20 @@ def sum_ele_gains(elevation_list, backwards=False):
 
 
 def pickle(graph, save_file):
+    """
+    Uses gpickle to save a networkx graph
+    
+    Parameters:
+    -----------
+    graph:
+        The networkx graph to save.
+    save_file: string
+        The path up to and including the file name, but exluding the extention.
+        
+    Returns:
+    --------
+    None
+    """
     print("Saving to file location {}.gpickle...".format(save_file))
     start = timer()
     nx.write_gpickle(graph, save_file + '.gpickle')
