db0edec906a64356593f8704526e15623e220a82
Ben <blah>
2017-12-09 15:00:31 -0500

    added import statements in driver, added __init__ to flask for importing

--- Driver.py
+++ Driver.py
@@ -1,6 +1,18 @@
 
+"""
+This is the driver class for the program. It has a composition relationship with the
+-GUI component
+-Algorithm back-end component
+-Data model (for algorithm)
+"""
+import a_star
+import model_generator
+import flask.app.routes
 
 class Driver:
     def __init__(self):
+
+
+
 
 if __name__ == '__main__':
