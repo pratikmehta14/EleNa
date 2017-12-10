ce0376012a791d33e3687ea909de209bae0d90ec
Ben <blah>
2017-12-10 14:12:42 -0500

    renamed tester file

--- Maximize.py
+++ Maximize.py
@@ -1,69 +1,76 @@
 import osmnx as ox, networkx as nx
 from heapq import heappush, heappop
 from itertools import count
+import copy
 
-
-
-def elevation_heuristic(graph, source, target):
-    return max(0, graph.nodes[target]['elevation'] - graph.nodes[source]['elevation'])
-
-def length_heuristic(graph, source, target):
-    s_y = graph.nodes[source]['y']
-    s_x = graph.nodes[source]['x']
-    t_y = graph.nodes[target]['y']
-    t_x = graph.nodes[target]['x']
-    return ox.euclidean_dist_vec(s_y, s_x, t_y, t_x)
 
 """
 Given a graph, networkx will generate the shortest path P from source to target based on length.
-It will then loop over the nodes P, starting from the second to last, and for each node it will 
-find all 
+
+It will then loop over the nodes P, and for each node it will  create a list L of all paths 
+from P_n to P_n+1 with some cutoff depth. 
+The graph handed to nx.all_simple_paths will have had all nodes P_0 through P_n-1 removed [IMPORTANT].
+It loops over L to find the path that has the properties:
+    1. ele_gain >= 0
+    2. length(P_0 -> P_n) + length(L[i]) + length(P_n+1 -> P_k)
+    3. L[i] has highest ele_gain in L
+    
+Upon finding this path P_new, it will be inserted between P_n and P_n+1 
 """
-def maximize(graph, source, target, percent_shortest_path,
-                       ele_heuristic, len_heuristic):
-    push = heappush
-    pop = heappop
-    counter = count()
+def maximize(graph, source, target, percent_shortest_path,):
     shortest_path = nx.shortest_path(graph, source, target, 'length')
+    working_path = copy.deepcopy(shortest_path)
+
     shortest_path_len = nx.shortest_path_length(graph, source, target, 'length')
     max_length = shortest_path_len * percent_shortest_path
 
-    # for i in range(len(shortest_path)-1):
+    #while we can still maximize ele-gain..
+    # while (pathLength(graph, working_path) < max_length):
+
+    offset = 0
+
+    #loop over nodes in the path
+    for i in range(len(shortest_path)-1):
+        currentNode = shortest_path[i]
+        nextNode = shortest_path[i+1]
+
+        #remove all nodes except currentNode and nextNode for the graph for this function
+        graph_truncated = copy.deepcopy(graph)
+        graph_truncated.remove_nodes_from(shortest_path[0:i]+shortest_path[i+2:])
+        interPaths = nx.all_simple_paths(graph_truncated, currentNode, nextNode, cutoff=10)
+
+        #find path in interPaths that has highest ele-gain
+        ele_gain_max = 0
+        biggestPath = []
+
+        for path in interPaths:
+            ele_gain=0
+
+            for node in range(len(path)-1):
+                ele_gain += graph[path[node]][path[node + 1]][0]['ele_gain']
+
+            if (ele_gain < 0): continue
+
+            #if the whole new path is longer than allowed, disregard it
+            if (pathLength(graph, working_path[0:offset] + path + working_path[offset+2:]) > max_length):
+                continue
+
+            if (ele_gain > ele_gain_max):
+                ele_gain_max = ele_gain
+                biggestPath = path
+
+        biggest_path_length = len(biggestPath)
+        if (biggest_path_length == 0):
+            biggestPath = [currentNode, nextNode]
+            biggest_path_length += 2
+        working_path = working_path[0:offset] + biggestPath + working_path[offset+2:]
+        offset += biggest_path_length - 1
+
+    return working_path
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#modified version of networkx's function, modified so that cutoff is a function of distance
-def all_simple_paths_multigraph(G, source, target, cutoff=None):
-    if cutoff < 1:
-        return
-    visited = [source]
-    stack = [(v for u, v in G.edges(source))]
-    while stack:
-        children = stack[-1]
-        child = next(children, None)
-        if child is None:
-            stack.pop()
-            visited.pop()
-        elif len(visited) < cutoff:
-            if child == target:
-                yield visited + [target]
-            elif child not in visited:
-                visited.append(child)
-                stack.append((v for u, v in G.edges(child)))
-        else:  # len(visited) == cutoff:
-            count = ([child] + list(children)).count(target)
-            for i in range(count):
-                yield visited + [target]
-            stack.pop()
-            visited.pop()
+def pathLength(graph, path):
+    path_length = 0
+    for node in range(len(path) - 1):
+        path_length += graph[path[node]][path[node + 1]][0]['length']
+    return path_length
---
+++ Tester_Maximize.py
@@ -0,0 +1,27 @@
+import Maximize
+import model_generator as mg
+import osmnx as ox, networkx as nx
+import googlemaps
+
+def pathGain(graph, path):
+    path_ele_gain = 0
+    for node in range(len(path) - 1):
+        path_ele_gain += graph[path[node]][path[node + 1]][0]['ele_gain']
+    return path_ele_gain
+
+if __name__ == '__main__':
+
+    city = nx.read_gpickle('amherst_graph01.gpickle')
+    nodes = city.nodes(data=True)
+
+
+    start = 66592882
+    target = 66592890
+
+
+    shortest_path = nx.shortest_path(city, start, target, 'length')
+    path = Maximize.maximize(city, start, target, 1.5)
+
+    print("elevation gain for this path is %d, elevation gain of shortest path is: %d" % (pathGain(city, path), pathGain(city, shortest_path)))
+
+    print('end of tester')
